15a7128e2ac9ccc2be5fe5cc3525a0bc83644153
MarioIvanovski <ivanovskimarioseptember2002@gmail.com>
2024-12-11 19:59:27 +0100

    Delete JavaScriptHomework directory

--- JavaScriptHomework/Homework_1/script.js
+++
@@ -1,5 +0,0 @@
-const pricePerPhone = 119.95;
-const quantity = 30;
-const taxRate = 0.05;
-const totalPrice = pricePerPhone * quantity * (1 + taxRate);
-console.log("Total Price including tax: $" ,totalPrice.toFixed(2));
