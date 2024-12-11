67b413668e6706f3048c32e8fa7421cad1f8fb18
MarioIvanovski <ivanovskimarioseptember2002@gmail.com>
2024-12-11 20:06:07 +0100

    Delete JavaScript_Homework directory

--- JavaScript_Homework/Homework_Class_4/Homework_3/script.js
+++
@@ -1,8 +0,0 @@
-function arrayToString(array)
-{
-    return array.join(' ');
-}
-
-let input = prompt("Enter a list of string separated by commas to form a sentance:");
-let array = input.split(",");
-console.log(arrayToString(array));
